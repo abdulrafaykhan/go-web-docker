226af478dfdd7cc48aa644b541bdee0d19a22beb
rafay <rafay@example.com>
2022-09-04 04:20:57 -0700

    Committing for the first time

---
+++ src/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	//This would match the following
+	// /sum/3/5
+	// /product/6/23
+
+	beego.Router("/:operation/:num1:int/:num2:int", &mainController{})
+	beego.Run()
+}
+
+type mainController struct {
+	beego.Controller
+}
+
+func (c *mainController) Get() {
+
+	// Obtain the values of the route parameters defined in the route
+	operation := c.Ctx.Input.Param(":operation")
+	num1, _ := strconv.Atoi(c.Ctx.Input.Param(":num1"))
+	num2, _ := strconv.Atoi(c.Ctx.Input.Param(":num2"))
+
+	// Set the values for use in the template
+	c.Data["operations"] = operation
+	c.Data["num1"] = num1
+	c.Data["num2"] = num2
+	c.TplName = "result.html"
+
+	// Perform the calculation depending upon the route
+	switch operation {
+	case "sum":
+		c.Data["result"] = add(num1, num2)
+	case "product":
+		c.Data["result"] = multiply(num1, num2)
+	case "quotient":
+		c.Data["result"] = divide(num1, num2)
+	case "difference":
+		c.Data["result"] = subtract(num1, num2)
+	default:
+		c.TplName = "invalid-route.html"
+	}
+}
+
+func add(n1, n2 int) int {
+	return n1 + n2
+}
+
+func multiply(n1, n2 int) int {
+	return n1 * n2
+}
+
+func divide(n1, n2 int) int {
+	return n1 / n2
+}
+
+func subtract(n1, n2 int) int {
+	return n1 - n2
+}
---
+++ src/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	if add(2, 5) != 7 {
+		t.Fail()
+	}
+	if add(2, 100) != 102 {
+		t.Fail()
+	}
+	if add(222, 100) != 322 {
+		t.Fail()
+	}
+}
+
+func TestProduct(t *testing.T) {
+	if multiply(2, 5) != 10 {
+		t.Fail()
+	}
+	if multiply(2, 100) != 200 {
+		t.Fail()
+	}
+	if multiply(222, 3) != 666 {
+		t.Fail()
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	if subtract(5, 3) != 2 {
+		t.Fail()
+	}
+	if subtract(6, 4) != 2 {
+		t.Fail()
+	}
+	if subtract(10, 7) != 3 {
+		t.Fail()
+	}
+}
